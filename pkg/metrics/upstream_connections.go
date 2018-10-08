918df8a564e7e77c3f7d395cd0803cfaa8a1d488
Hylke Visser <htdvisser@gmail.com>
2018-10-08 15:05:49 +0200

    Ignore "authorized_keys" prefix in metric

--- pkg/metrics/upstream_connections.go
+++ pkg/metrics/upstream_connections.go
@@ -1,6 +1,10 @@
 package metrics
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 var (
 	connectionsStarted = prometheus.NewCounterVec(
@@ -29,16 +33,19 @@ func init() {
 
 // RegisterStartForward registers the start of an SSH forwarding connection.
 func RegisterStartForward(pubKey, upstream string) {
+	pubKey = strings.TrimPrefix(pubKey, "authorized_keys_")
 	connectionsStarted.WithLabelValues(pubKey, upstream).Inc()
 }
 
 // RegisterEndForward registers the end of an SSH forwarding connection.
 func RegisterEndForward(pubKey, upstream string) {
+	pubKey = strings.TrimPrefix(pubKey, "authorized_keys_")
 	connectionsEnded.WithLabelValues(pubKey, upstream).Inc()
 }
 
 // InitForward initializes the SSH forwarding metrics for a given pubKey and upstream.
 func InitForward(pubKey, upstream string) {
+	pubKey = strings.TrimPrefix(pubKey, "authorized_keys_")
 	connectionsStarted.WithLabelValues(pubKey, upstream).Add(0)
 	connectionsEnded.WithLabelValues(pubKey, upstream).Add(0)
 }
